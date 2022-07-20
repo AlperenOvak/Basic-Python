9fd41696936f88e6436a8a94c83e5f53f7a0d14e
AlperenOvak <96688864+AlperenOvak@users.noreply.github.com>
2022-07-20 19:01:55 +0300

    Update flatten.py

--- flatten.py
+++ flatten.py
@@ -1,7 +1,7 @@
 l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5] 
 lnew=[] 
-def is_i_list(i): #this function finds the objects which are list
-    if type(i)==list:
+def is_i_list(a): #this function finds the objects which are list
+    if type(a)==list:
         return True
 def flatten(l): 
     for i in l:
